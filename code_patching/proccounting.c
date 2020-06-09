33dc24b3ed32ab92671fca23defb77da5d657e32
Keren Chen <kerenchen@melange.cs.wisc.edu>
2020-06-09 11:09:02 -0500

    pause after first run of interestingproc

--- code_patching/proccounting.c
+++ code_patching/proccounting.c
@@ -18,6 +18,8 @@ using namespace std;
 
 // DyninstAPI includes
 #include "BPatch.h"
+#include "BPatch_addressSpace.h"
+#include "BPatch_process.h"
 #include "BPatch_binaryEdit.h"
 #include "BPatch_function.h"
 #include "BPatch_point.h"
@@ -30,13 +32,24 @@ BPatch bpatch;
 
 // configuration options
 static char *pathname = NULL;
-static char *outbinary = "output";
+//static char *outbinary = "output";
 static const char **args = NULL;
 
 bool parseArgs (int argc, char *argv[])
 {
     return true;
 }
+
+bool instrumentCounter()
+{
+	return true;
+}
+
+bool instrumentPrint()
+{
+        return true;
+}
+
 
 /*
  *
@@ -47,24 +60,29 @@ int main (int argc, char *argv[])
         	return EXIT_FAILURE;
 
 	pathname = argv[1];
+
+
+
+
 	//identifies the application process to be modified
 	//BPatch_process *appProc = bpatch.processCreate(pathname, args);
 	//BPatch_binaryEdit *appBin = bpatch.openBinary(pathname, true);
 	BPatch_process *appProc = bpatch.processCreate(pathname, args);
 	BPatch_addressSpace *app = appProc;
 	BPatch_image *appImage = app->getImage();
-
-
 	//std::vector<BPatch_function *> functions;
 	//appThread->bpatch.createProcess(pathname, args); 
 	//BPatch_image *appImage = app->getImage(); 
 
+
+
+
+	//insert the call counter at the start of interesting prcedure
 	vector<BPatch_function*> functions;
 	appImage->findFunction("InterestingProcedure", functions);
 	//vector < BPatch_function * >::iterator funcIter = functions.begin();
 	std::vector<BPatch_point *> *points;
 	points = functions[0]->findPoint(BPatch_locEntry);
-
 	//BPatch_function * headFunc = *funcIter;
 	//std::vector<BPatch_point*>* points = 
 	//	headFunc->findPoint(BPatch_locEntry);
@@ -80,11 +98,46 @@ int main (int argc, char *argv[])
 	        return false;
 	}
 
-	// Output the instrumented binary
-    appProc->continueExecution();
-while (!appProc->isTerminated()) {
-	bpatch.waitForStatusChange();
-}
+
+
+	//insert the counter printer at the end of main function
+	vector<BPatch_function*> func2;
+        appImage->findFunction("main", func2);
+	std::vector<BPatch_point *> *exitPoint;
+	exitPoint = func2[0]->findPoint(BPatch_exit);
+
+	if (!exitPoint || exitPoint->size() == 0) {
+        	fprintf(stderr, "No exit points for main\n");
+        	exit(1);
+    	}
+
+   	 std::vector<BPatch_snippet*> printfArgs;
+	BPatch_snippet* fmt =
+        new BPatch_constExpr("InterestingProcedure called %d times\n");
+       	printfArgs.push_back(fmt);
+	BPatch_variableExpr* var = appImage->findVariable("intCounter");
+	printfArgs.push_back(var);
+
+	std::vector<BPatch_function*> printfFuncs;
+	appImage->findFunction("printf", printfFuncs);
+
+
+	BPatch_funcCallExpr printfCall(*(printfFuncs[0]), printfArgs);
+	//BPatch_breakPointExpr stop();
+	//vector<BPatch_function*> functions2;
+	//appImage->findFunction("main", functions2);
+	//std::vector<BPatch_point *> *points2;
+	//points2 = functions2[0]->findPoint(BPatch_locEntry);
+	//app->insertSnippet(BPatch_breakPointExpr(),  *points2, BPatch_callBefore, BPatch_lastSnippet);
+	
+	
+	
+	
+	// wait for the program to finish
+	appProc->continueExecution();
+	while (!appProc->isTerminated()) {
+		bpatch.waitForStatusChange();
+	}
 
 	return EXIT_SUCCESS;
 }
