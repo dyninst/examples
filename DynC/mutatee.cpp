1aa16cbacec25effe2db1f897f9c0fac718bc172
Tim Haines <thaines@cs.wisc.edu>
2021-10-24 12:39:07 -0500

    Add DynC example

---
+++ DynC/mutatee.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+int hi = 0;
+int zarray[0];
+int array[5] = {1, 2, 3, 4, 5};
+int array2[5] = {6, 7, 8, 9, 10};
+int arrayField = array[2];
+
+struct myStructType {
+  int i;
+  char const *s;
+  char const *sa[4];
+} mystruct = {3, "house", {"how", "now", "brown", "cow"}};
+
+int count(int i);
+
+int zomg = 2;
+
+int main() {
+
+  int i = 0;
+  int r = 0;
+  while (i < 10) {
+    i = count(i);
+    ++hi;
+    r = hi * 10;
+  }
+}
+
+void hello() {}
+
+int count(int i) {
+  if (i % 2 == 0) {
+    hello();
+  }
+  array[i % 5]++;
+  return i + 1;
+}
+
+int count(int i, char *n) {
+  printf("%s", n);
+  return i + 1;
+}
+
+int printfWrapper(char *s) {
+  printf("%s", s);
+  return 1;
+}
+
+int count(char *s) {
+  printf("%s\n", s);
+  return 1;
+}
---
+++ DynC/mutator.cpp
@@ -0,0 +1,111 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include "BPatch.h"
+#include "BPatch_function.h"
+#include "BPatch_process.h"
+#include "BPatch_snippet.h"
+#include "dynC.h"
+
+#define STATEMENT_PATH "testStatements"
+#define STATEMENT_PATH_2 "testStatements2"
+
+const char *MUTATEE_PATH = "dync_mutatee";
+const char *MUTATEE_ARGS[3];
+const char *MODULE_NAME = "mutatee.cpp";
+
+BPatch bpatch;
+
+int main() {
+
+  std::ifstream myfile(STATEMENT_PATH);
+  if(!myfile) {
+	  std::cerr << "Unable to open '" << STATEMENT_PATH << "'\n";
+	  return -1;
+  }
+
+  std::ifstream myfile2(STATEMENT_PATH_2);
+  if(!myfile2) {
+	  std::cerr << "Unable to open '" << STATEMENT_PATH_2 << "'\n";
+	  return -1;
+  }
+
+  FILE *myCFILE = fopen(STATEMENT_PATH, "r");
+
+  BPatch_addressSpace *appProc;
+  bool rewrite = false;
+
+  if (rewrite) {
+    appProc = bpatch.openBinary(MUTATEE_PATH, true);
+  } else {
+    appProc = bpatch.processCreate(MUTATEE_PATH, MUTATEE_ARGS);
+  }
+  if (!appProc) {
+	  std::cerr << "Unable to create an address space\n";
+	  return -1;
+  }
+
+  BPatch_image *appImage = appProc->getImage();
+  BPatch_module *mutatee = appImage->findModule(MODULE_NAME);
+
+  if (!mutatee) {
+    std::cerr << "Bad Mutatee!\n";
+    return -1;
+  }
+
+  appProc->malloc(*appImage->findType("long"), std::string("globalVar"));
+
+  const std::vector<BPatch_function *> *functions = mutatee->getProcedures();
+  std::vector<BPatch_point *> *entry_points = (*functions)[0]->findPoint(BPatch_entry);
+  std::vector<BPatch_point *> *exit_points = (*functions)[0]->findPoint(BPatch_exit);
+
+  for (auto *f : *mutatee->getProcedures()) {
+    entry_points->push_back((*f->findPoint(BPatch_entry))[0]);
+    exit_points->push_back((*f->findPoint(BPatch_exit))[0]);
+  }
+
+  /////////////////////////////////////////////////////////
+  std::map<BPatch_point *, BPatch_snippet *> *entry_snippets, exit_snippets;
+  entry_snippets = dynC_API::createSnippet(myCFILE, *entry_points);
+
+  if (!entry_snippets) {
+    std::cerr << "entry_snippets is null.\n";
+    return -1;
+  }
+
+  for (auto const &entry : *entry_snippets) {
+    if (!entry.first) {
+      std::cerr << "Entry point is null.\n";
+      return -1;
+    }
+    if (!entry.second) {
+      std::cerr << "Entry snippet is null.\n";
+      return -1;
+    }
+    std::cout << "Snippet inserted\n";
+    std::cout << "Point's function is " << entry.first->getFunction()->getName(), "\n";
+    BPatchSnippetHandle *handle = appProc->insertSnippet(*entry.second, *entry.first);
+    std::cout << "Handle is " << ((!handle) ? "null" : "not null") << "\n";
+  }
+
+  if (!rewrite) {
+    BPatch_process *aProc = static_cast<BPatch_process *>(appProc);
+    aProc->continueExecution();
+
+    while (!aProc->isTerminated()) {
+      bpatch.waitForStatusChange();
+    }
+
+    if (aProc->terminationStatus() == ExitedNormally) {
+      printf("Application exited with code %d\n", aProc->getExitCode());
+    } else if (aProc->terminationStatus() == ExitedViaSignal) {
+      printf("!!! Application exited with signal %d\n", aProc->getExitSignal());
+    } else {
+      printf("Unknown application exit\n");
+    }
+  } else {
+    BPatch_binaryEdit *aProc = static_cast<BPatch_binaryEdit *>(appProc);
+    aProc->writeFile("myMutatee.out");
+  }
+}
