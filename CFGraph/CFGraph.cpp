27d10f80024b9636e1e907b3f55bab300240a2ae
Tim Haines <thaines.astro@gmail.com>
2022-03-09 22:28:08 -0600

    Update call graph to remove compiler-generated functions from output (#29)
    * Update call graph to remove compiler-generated functions from output
    
    This also modernizes the code.

--- CFGraph/CFGraph.cpp
+++ CFGraph/CFGraph.cpp
@@ -2,95 +2,108 @@
 // control flow graph of the provided binary.
 //
 // Improvements by E. Robbins (er209 at kent dot ac dot uk)
-//
 
-#include <stdio.h>
-#include <map>
-#include <vector>
-#include <unordered_map>
-#include <sstream>
-#include "CodeObject.h"
 #include "CFG.h"
+#include "CodeObject.h"
+#include <sstream>
+#include <string>
+#include <unordered_set>
+#include <vector>
 
-using namespace std;
-using namespace Dyninst;
-using namespace ParseAPI;
+namespace dp = Dyninst::ParseAPI;
+namespace st = Dyninst::SymtabAPI;
 
-int main(int argc, char * argv[])
-{
-	map<Address, bool> seen;
-	vector<Function *> funcs;
-	SymtabCodeSource *sts;
-	CodeObject *co;
+int main(int argc, char *argv[]) {
+  if (argc < 2 || argc > 3) {
+    std::cerr << "Usage: " << argv[0] << " executable\n";
+    return -1;
+  }
 
-	// Create a new binary code object from the filename argument
-	sts = new SymtabCodeSource( argv[1] );
-	co = new CodeObject( sts );
+  auto *sts = new dp::SymtabCodeSource(argv[1]);
+  auto *co = new dp::CodeObject(sts);
 
-	// Parse the binary
-	co->parse();
-	cout << "digraph G {" << endl;
+  // Parse the binary
+  co->parse();
 
-	// Print the control flow graph
-	const CodeObject::funclist& all = co->funcs();
-	auto fit = all.begin();
-	for(int i = 0; fit != all.end(); ++fit, i++) { // i is index for clusters
-		Function *f = *fit;
+  std::cout << "digraph G {" << '\n';
 
-		// Make a cluster for nodes of this function
-		cout << "\t subgraph cluster_" << i
-		     << " { \n\t\t label=\""
-		     << f->name()
-		     << "\"; \n\t\t color=blue;" << endl;
+  // Print the control flow graph
+  dp::CodeObject::funclist all = co->funcs();
 
-		cout << "\t\t\"" << hex << f->addr() << dec
-		     << "\" [shape=box";
-		if (f->retstatus() == NORETURN)
-			cout << ",color=red";
-		cout << "]" << endl;
+  // Remove compiler-generated and system functions
+  {
+    auto ignore = [&all](dp::Function const *f) {
+      auto const &name = f->name();
+      bool const starts_with_underscore = name[0] == '_';
+      bool const ends_with_underscore = name[name.length() - 1] == '_';
+      bool const is_dummy = name == "frame_dummy";
+      bool const is_clones = name.find("tm_clones") != std::string::npos;
+      return starts_with_underscore || ends_with_underscore || is_dummy ||
+             is_clones;
+    };
 
-		// Label functions by name
-		cout << "\t\t\"" << hex << f->addr() << dec
-		     << "\" [label = \""
-		     << f->name() << "\\n" << hex << f->addr() << dec
-		     << "\"];" << endl;
+    // 'funclist' is a std::set which has only const iterators
+    auto i = all.begin();
+    while (i != all.end()) {
+      if (ignore(*i)) {
+        i = all.erase(i);
+      } else {
+        ++i;
+      }
+    }
+  }
 
-		stringstream edgeoutput;
+  std::unordered_set<Dyninst::Address> seen;
 
-		auto bit = f->blocks().begin();
-		for( ; bit != f->blocks().end(); ++bit) {
-			Block *b = *bit;
-			// Don't revisit blocks in shared code
-			if(seen.find(b->start()) != seen.end())
-				continue;
+  int cluster_index = 0;
+  for (auto const *f : all) {
+    // Make a cluster for nodes of this function
+    std::cout << "\t subgraph cluster_" << cluster_index << " { \n\t\t label=\""
+              << f->name() << "\"; \n\t\t color=blue;" << '\n';
 
-			seen[b->start()] = true;
+    std::cout << "\t\t\"" << std::hex << f->addr() << std::dec
+              << "\" [shape=box";
 
-			cout << "\t\t\"" << hex << b->start() << dec <<
-				"\";" << endl;
+    if (f->retstatus() == dp::NORETURN)
+      std::cout << ",color=red";
 
-			auto it = b->targets().begin();
-			for( ; it != b->targets().end(); ++it) {
-				if(!*it) continue;
-				std::string s = "";
-				if((*it)->type() == CALL)
-					s = " [color=blue]";
-				else if((*it)->type() == RET)
-					s = " [color=green]";
+    std::cout << "]" << '\n';
 
-				// Store the edges somewhere to be printed outside of the cluster
-				edgeoutput << "\t\""
-					   << hex << (*it)->src()->start()
-					   << "\" -> \""
-					   << (*it)->trg()->start()
-					   << "\"" << s << endl;
-			}
-		}
-		// End cluster
-		cout << "\t}" << endl;
+    // Label functions by name
+    std::cout << "\t\t\"" << std::hex << f->addr() << std::dec
+              << "\" [label = \"" << f->name() << "\\n"
+              << std::hex << f->addr() << std::dec << "\"];" << '\n';
 
-		// Print edges
-		cout << edgeoutput.str() << endl;
-	}
-	cout << "}" << endl;
+    std::stringstream edgeoutput;
+
+    for (dp::Block *b : f->blocks()) {
+      // Don't revisit blocks in shared code
+      if (seen.count(b->start()) > 0)
+        continue;
+      seen.insert(b->start());
+
+      std::cout << "\t\t\"" << std::hex << b->start() << std::dec << "\";"
+                << '\n';
+
+      for (dp::Edge *e : b->targets()) {
+        if (!e)
+          continue;
+        std::string s;
+        if (e->type() == dp::CALL)
+          s = " [color=blue]";
+        else if (e->type() == dp::RET)
+          s = " [color=green]";
+
+        // Store the edges somewhere to be printed outside of the cluster
+        edgeoutput << "\t\"" << std::hex << e->src()->start() << "\" -> \""
+                   << e->trg()->start() << "\"" << s << '\n';
+      }
+    }
+    // End cluster
+    std::cout << "\t}" << '\n';
+
+    // Print edges
+    std::cout << edgeoutput.str() << '\n';
+  }
+  std::cout << "}" << '\n';
 }
