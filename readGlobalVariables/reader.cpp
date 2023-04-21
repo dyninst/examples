a103af92e0334fb962aa87070d1eaa91a66e4379
Tim Haines <thaines.astro@gmail.com>
2023-04-20 20:29:02 -0500

    Add readGlobalVariables (#34)
    An example on how to read global variables using Symtab.

---
+++ readGlobalVariables/reader.cpp
@@ -0,0 +1,35 @@
+#include "Symtab.h"
+#include "Variable.h"
+#include "Type.h"
+#include <vector>
+#include <iostream>
+
+int main() {
+  namespace ds = Dyninst::SymtabAPI;
+  ds::Symtab *sym;
+  if(!ds::Symtab::openFile(sym, "libglobals.so")) {
+    std::cout << "Failed to open 'libtest.so'\n";
+    return -1;
+  }
+  
+  for(auto var_name : {"x", "y", "z"}) {
+    std::cout << "Looking for '" << var_name << "'\n";
+    std::vector<ds::Variable*> vars;
+    sym->findVariablesByName(vars, var_name);
+    for(auto *v : vars) {
+      std::cout << *v << "\n\n";
+    }
+    std::cout << "Found " << vars.size() << " variables named " << var_name << "\n";
+    auto *v = vars[0];
+    auto type = v->getType(ds::Type::do_share_t{});
+    std::cout << "Type name: " << type->getName() << "\n";
+
+    while(auto *arr = type->getArrayType()) {
+      std::cout << "Name: " << arr->getName() << "\n"
+                << "Bounds: " << arr->getLow() << "," << arr->getHigh()
+                << "\n\n";
+      type = arr->getBaseType(ds::Type::do_share_t{});
+      if(!type) break;
+    }
+  }
+}
