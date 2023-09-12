9c590bfbe21ba3459f227699aec4d7f9bec8a893
Tim Haines <thaines.astro@gmail.com>
2023-09-12 00:20:20 -0500

    Examples from dataflowAPI docs

---
+++ dataflowAPI/liveness.cpp
@@ -0,0 +1,33 @@
+#include "liveness.h"
+#include "Location.h"
+#include "bitArray.h"
+
+#include <cstdio>
+
+namespace pa = Dyninst::ParseAPI;
+
+void LivenessAnalysis(pa::Function *f, pa::Block *b) {
+  // Construct a liveness analyzer based on the address width of the mutatee.
+  // 32-bit code and 64-bit code have different ABI.
+  LivenessAnalyzer la(f->obj()->cs()->getAddressWidth());
+
+  // Construct a liveness query location
+  pa::Location loc(f, b);
+
+  // Query live registers at the block entry
+  bitArray liveEntry;
+  if (!la.query(loc, LivenessAnalyzer::Before, liveEntry)) {
+    printf("Cannot look up live registers at block entry\n");
+  }
+
+  printf("There are %lu registers live at the block entry\n", liveEntry.count());
+
+  // Query live register at the block exit
+  bitArray liveExit;
+  if (!la.query(loc, LivenessAnalyzer::After, liveExit)) {
+    printf("Cannot look up live registers at block exit\n");
+  }
+
+  printf("rbx is live or not at the block exit: %d\n",
+         liveExit.test(la.getIndex(Dyninst::x86_64::rbx)));
+}
---
+++ dataflowAPI/symbolic_evaluation.cpp
@@ -0,0 +1,52 @@
+#include "SymEval.h"
+#include "slicing.h"
+
+namespace df = Dyninst::DataflowAPI;
+
+// We extend the default ASTVisitor to check whether the AST is a constant
+class ConstVisitor : public Dyninst::ASTVisitor {
+public:
+  bool resolved;
+  Dyninst::Address target;
+  ConstVisitor() : resolved(true), target(0) {}
+
+  // We reach a constant node and record its value
+  Dyninst::AST::Ptr visit(df::ConstantAST *ast) override {
+    target = ast->val().val;
+    return Dyninst::AST::Ptr();
+  };
+
+  // If the AST contains a variable
+  // or an operation, then the control flow target cannot
+  // be resolved through constant propagation
+  Dyninst::AST::Ptr visit(df::VariableAST *) override {
+    resolved = false;
+    return Dyninst::AST::Ptr();
+  };
+  Dyninst::AST::Ptr visit(df::RoseAST *ast) override {
+    resolved = false;
+
+    // Recursively visit all children
+    unsigned totalChildren = ast->numChildren();
+    for (unsigned i = 0; i < totalChildren; ++i) {
+      ast->child(i)->accept(this);
+    }
+    return Dyninst::AST::Ptr();
+  };
+};
+
+Dyninst::Address ExpandSlice(Dyninst::GraphPtr slice, Dyninst::Assignment::Ptr pcAssign) {
+  df::Result_t symRet;
+  df::SymEval::expand(slice, symRet);
+
+  // We get AST representing the jump target
+  Dyninst::AST::Ptr pcExp = symRet[pcAssign];
+
+  // We analyze the AST to see if it can actually be resolved by constant
+  // propagation
+  ConstVisitor cv;
+  pcExp->accept(&cv);
+  if (cv.resolved)
+    return cv.target;
+  return 0;
+}
