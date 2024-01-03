3528a7224269f1a9759e5fb2e1e9bb84f931b543
Tim Haines <thaines.astro@gmail.com>
2024-01-03 09:43:41 -0600

    Add parseAPI (#45)

---
+++ parseAPI/edgePredicate.cpp
@@ -0,0 +1,29 @@
+#include "CFG.h"
+
+#include <vector>
+
+namespace dp = Dyninst::ParseAPI;
+
+void edge(dp::Function* func) {
+  std::vector<dp::Block*> work;
+  work.push_back(func->entry());
+
+  dp::Intraproc intra_pred; // ignore calls, returns
+  dp::Interproc inter_pred;
+
+  while(!work.empty()) {
+    dp::Block* block = work.back();
+    work.pop_back();
+
+    dp::Block::edgelist const& targets = block->targets();
+
+    for(dp::Edge *e : targets) {
+      if(intra_pred.pred_impl(e)) {
+        // Examine the Intraprocedural edge
+      }
+      if(inter_pred.pred_impl(e)) {
+        // Examine the Interprocedural edge
+      }
+    }
+  }
+}
---
+++ parseAPI/loopAnalysis.cpp
@@ -0,0 +1,23 @@
+#include "CFG.h"
+#include <vector>
+
+namespace dp = Dyninst::ParseAPI;
+
+void GetLoopInFunc(dp::Function *f) {
+  std::vector<dp::Loop*> loops;
+  f->getLoops(loops);
+
+  for(dp::Loop *loop : loops) {
+    // Get all the entry blocks
+    std::vector<dp::Block*> entries;
+    loop->getLoopEntries(entries);
+
+    // Get all the blocks in the loop
+    std::vector<dp::Block*> blocks;
+    loop->getLoopBasicBlocks(blocks);
+
+    // Get all the back edges in the loop
+    std::vector<dp::Edge*> backEdges;
+    loop->getBackEdges(backEdges);
+  }
+}
