b0c779458221147ea55b1e60d5f06e593fb0931b
Tim Haines <thaines.astro@gmail.com>
2024-01-25 00:02:41 -0600

    Add stackwalker snippets from docs

---
+++ stackwalker/complex3rdPartyWalk.cpp
@@ -0,0 +1,43 @@
+#include "frame.h"
+#include "procstate.h"
+#include "walker.h"
+
+#include <thread>
+#include <vector>
+
+#include <sys/select.h>
+
+// Collect a stack walk every five seconds.
+
+namespace sw = Dyninst::Stackwalker;
+
+void walk(Dyninst::PID pid) {
+  auto* walker = sw::Walker::newWalker(pid);
+  std::vector<sw::Frame> swalk;
+
+  struct timeval timeout;
+  timeout.tv_sec = 5;
+  timeout.tv_usec = 0;
+
+  for(;;) {
+    walker->walkStack(swalk);
+    int max = 1;
+    fd_set readfds, writefds, exceptfds;
+    FD_ZERO(&readfds);
+    FD_ZERO(&writefds);
+    FD_ZERO(&exceptfds);
+    FD_SET(sw::ProcDebug::getNotificationFD(), &readfds);
+
+    for(;;) {
+      int result = select(max, &readfds, &writefds, &exceptfds, &timeout);
+      if(FD_ISSET(sw::ProcDebug::getNotificationFD(), &readfds)) {
+        // Debug event
+        sw::ProcDebug::handleDebugEvent();
+      }
+      if(result == 0) {
+        // Timeout
+        break;
+      }
+    }
+  }
+}
---
+++ stackwalker/determineWalkerParty.cpp
@@ -0,0 +1,17 @@
+#include "walker.h"
+#include "procstate.h"
+
+// How to determine if first-party or third-party walker
+
+namespace sw = Dyninst::Stackwalker;
+
+int main() {
+  auto* walker = sw::Walker::newWalker();
+  auto* debugger = dynamic_cast<sw::ProcDebug*>(walker->getProcessState());
+
+  if(debugger) {
+   //3rd party walker
+  } else {
+   //1st party walker
+  }
+}
